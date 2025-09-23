91f51d0e747ce4da10be939864d2d3ad1244c665
yannis <yannisnyoka@gmail.com>
2025-09-23 17:10:47 +0200

    Adding Our_Services components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import About_Us from "./pages/About_Us";
 import Contact_Us from "./pages/Contact_Us";
+import Our_Services from "./pages/Our_Services";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Our_Services />} />
           <Route path="/aboutUs" element={<About_Us />} />
           <Route path="/contactUs" element={<Contact_Us />} />
         </Routes>
