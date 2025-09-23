76ccee660e3c6f99f0872c728f1aab45bc5e1563
Britney Nunes <britneynunes8@gmail.com>
2025-09-23 12:29:44 +0200

    Finished styling the NavBar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import "./Navbar.module.css"
-// import "../assets"
+import styles from './Navbar.module.css'
+import logo from '../assets/ActivemediaLogo.png'
 
 function Navbar() {
   return (
-    <>
-    <div className='styles.navBar'>
-      <ol className='styles.navList'>
-        <li>Logo</li>
-        <li>Software & Development</li>
-        <li>Design & Marketing</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
+    <div className={styles.navBar}>
+      <img src={logo} alt="Active Media Logo" className={styles.logo} />
+      <ol className={styles.navList}>
+        <li className={styles.navItem}>Software & Development</li>
+        <li className={styles.navItem}>Design & Marketing</li>
+        <li className={styles.navItem}>About Us</li>
+        <li className={styles.navItem}>Contact Us</li>
       </ol>
     </div>
-    </>
   )
 }
 
