da6adbd70967b3181a39117a57fe24585b3ad118
Britney Nunes <britneynunes8@gmail.com>
2025-09-23 11:18:59 +0200

    starting with NavBar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./Navbar.module.css"
+// import "../assets"
 
 function Navbar() {
   return (
-    <div>
-      
+    <>
+    <div className='styles.navBar'>
+      <ol className='styles.navList'>
+        <li>Logo</li>
+        <li>Software & Development</li>
+        <li>Design & Marketing</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+      </ol>
     </div>
+    </>
   )
 }
 
