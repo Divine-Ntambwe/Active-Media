f28218e92266d6c9c9302591c7544d45e010228d
HilariousElk1660 <jonathanobi47@gmail.com>
2025-09-23 12:00:29 +0200

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import About_Us from "./pages/About_Us";
 import Contact_Us from "./pages/Contact_Us";
+import Our_Services from "./pages/Our_Services";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/aboutUs" element={<About_Us />} />
+          <Route path="/services" element={<Our_Services />} />
           <Route path="/contactUs" element={<Contact_Us />} />
         </Routes>
       </Router>
--- src/pages/Our_Services.jsx
+++ src/pages/Our_Services.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Our_Services.module.css"
+import Navbar from '../component/Navbar.jsx'
 
 function Our_Services() {
   return (
     <div>
-      
+      <Navbar />
     </div>
   )
 }
