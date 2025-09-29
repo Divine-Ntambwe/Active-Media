ba0efca250449ad03e6d28c6f180f15f798502c5
Britney Nunes <britneynunes8@gmail.com>
2025-09-29 17:31:06 +0200

    Fixing App Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,20 @@ import "./App.css";
 import Home from "./pages/Home";
 import About_Us from "./pages/About_Us";
 import Contact_Us from "./pages/Contact_Us";
+import Recent_Work from "./pages/Our_Work";
 import Cloud_Based_Solutions from "./pages/Cloud_Based_Solutions";
 import Navbar from "./component/Navbar";
 
 
 function App() {
   return (
-    <> 
-
-    <Router>
-    <Navbar/>
+    <>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<About_Us />} />
           <Route path="/contact-us" element={<Contact_Us />} />
+          <Route path= '/ourwork' element= {<Recent_Work/>}/>
         </Routes>
-    </Router>
     </>
   );
 }
