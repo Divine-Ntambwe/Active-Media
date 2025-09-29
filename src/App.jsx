12669be5b682adfb074adfde739cd1e01de1a4f8
Divine Ntambwe <divinentambwe28@gmaail.com>
2025-09-29 11:16:15 +0200

    merged britney's branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,21 @@ import Home from "./pages/Home";
 import About_Us from "./pages/About_Us";
 import Contact_Us from "./pages/Contact_Us";
 import Our_Services from "./pages/Our_Services";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
-    <>
+    <> 
+
+    <Router>
+    <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<About_Us />} />
           <Route path="/contact-us" element={<Contact_Us />} />
           <Route path="/our-services" element={<Our_Services />} />
         </Routes>
+    </Router>
     </>
   );
 }
