a7e68955c4f9f28fee72a04ead3d6543f8936f38
christophermossi <mossichris18@iCloud.com>
2025-09-29 15:32:27 +0200

    finished the divs part

--- src/pages/Digital_Marketing.jsx
+++ src/pages/Digital_Marketing.jsx
@@ -7,29 +7,29 @@ const Digital_Marketing = () => {
     const BGImg = useRef();
     const heading = useRef();
     const par = useRef();
-  useEffect(() => {
-    const onScroll = () => {
-      const y = window.scrollY;
-      if (y > lastY) {
-        BGImg.current.style.top = "200px"
-        heading.current.style.animationPlayState = "running"
-        heading.current.style.animationFillMode = "forwards"
+  // useEffect(() => {
+  //   const onScroll = () => {
+  //     const y = window.scrollY;
+  //     if (y > lastY) {
+  //       BGImg.current.style.top = "200px"
+  //       heading.current.style.animationPlayState = "running"
+  //       heading.current.style.animationFillMode = "forwards"
 
-        par.current.style.animationPlayState = "running"
-        par.current.style.animationFillMode = "forwards"
+  //       par.current.style.animationPlayState = "running"
+  //       par.current.style.animationFillMode = "forwards"
         
-      }
-      else {
-      heading.current.style.animationFillMode = "backwards"
-      par.current.style.animationFillMode = "backwards"
-      BGImg.current.style.top = "-200px"
-      setLastY(y);
+  //     }
+  //     else {
+  //     heading.current.style.animationFillMode = "backwards"
+  //     par.current.style.animationFillMode = "backwards"
+  //     BGImg.current.style.top = "-200px"
+  //     setLastY(y);
      
-    }
-    };
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [lastY]);
+  //   }
+  //   };
+  //   window.addEventListener("scroll", onScroll);
+  //   return () => window.removeEventListener("scroll", onScroll);
+  // }, [lastY]);
     return (
     <div className={styles.container}>
         <img ref={BGImg} className={styles.digitalBackground} src="./backgroundLines.png"/>
@@ -85,6 +85,69 @@ const Digital_Marketing = () => {
         </ul>
       </div>
     </div>
+
+     {/* Card 3 */}
+      <div className={styles.cardThree}>
+        <div className={styles.iconBox}>
+        </div>
+        <h2 className={styles.title}>Flyer Design</h2>
+        <p className={styles.text}>
+          Eye-catching designs that spread your message and attract attention
+        </p>
+        <ul className={styles.list}>
+          <li>Bold Visual</li>
+          <li>Clear Messaging</li>
+          <li>Ready to Print</li>
+        </ul>
+      </div>
+
+      {/* Card 4 */}
+      <div className={styles.cardFour}>
+        <div className={styles.iconBox}>
+        </div>
+        <h2 className={styles.title}>Corporate Identity Design</h2>
+        <p className={styles.text}>
+          We build a cohesive visual identity that reflects your brand value 
+          across all platform 
+        </p>
+        <ul className={styles.list}>
+          <li>Consistent Branding</li>
+          <li>Professional Look</li>
+          <li>Custom Element</li>
+        </ul>
+      </div>
+
+         {/* Card 5 */}
+      <div className={styles.cardFive}>
+        <div className={styles.iconBox}>
+        </div>
+        <h2 className={styles.title}>Logo Redraw</h2>
+        <p className={styles.text}>
+          We recreate your logo in high quality,keeping the original look sharp and 
+          professional
+        </p>
+        <ul className={styles.list}>
+          <li>High Resolution</li>
+          <li>Clean Lines</li>
+          <li>Print & Web Ready</li>
+        </ul>
+      </div>
+
+       {/* Card 6 */}
+      <div className={styles.cardSix}>
+        <div className={styles.iconBox}>
+        </div>
+        <h2 className={styles.title}>Brochure Design</h2>
+        <p className={styles.text}>
+          We craft information and visual appealing brochures that showcase your
+          brand and services
+        </p>
+        <ul className={styles.list}>
+          <li>Creative Layouts</li>
+          <li>Clear Content</li>
+          <li>Print-Ready Files</li>
+        </ul>
+      </div>
       </div>
     </div>
   );
