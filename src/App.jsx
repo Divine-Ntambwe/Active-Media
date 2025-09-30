ada18713a75f5cdb1923bb1b2331c466b4eca61a
unknown <wilfredmvula@icloud.com>
2025-09-30 14:51:07 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About_Us from "./pages/About_Us";
 import Contact_Us from "./pages/Contact_Us";
 import Cloud_Based_Solutions from "./pages/Cloud_Based_Solutions";
 import Navbar from "./component/Navbar";
+import ECommercePage from "./pages/ECommercePage";
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
     <Router>
     <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ECommercePage />} />
           <Route path="/about-us" element={<About_Us />} />
           <Route path="/contact-us" element={<Contact_Us />} />
         </Routes>
