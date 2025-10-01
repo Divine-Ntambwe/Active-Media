1ef8e9c54cb393d7f1dad8794c69ff04fae4e05e
Divine Ntambwe <divinentambwe28@gmaail.com>
2025-10-01 11:41:58 +0200

    fixed merge with nicky branch div

--- src/pages/About_Us.jsx
+++ src/pages/About_Us.jsx
@@ -73,41 +73,7 @@ export default function About_Us() {
       <div className={`${styles.aboutDiv} ${styles.div2}`}>
         
       {/* Carmel Framework (the start) */}
-      <div className={styles.container}>
-        <div className={styles.text}>
-        <h5>Team</h5>
-        <p className={styles.teamText}>
-        Active Media consists of a team of<br></br>
-        people that are passionate about what<br></br>   
-        they do and strive to ensure that only only the<br></br>
-        very best service is offered to our <br></br>
-        clients. We stand by our company ethics<br></br>
-        -ensuring honesty, authenticity and <br></br>
-        excellence. You have ideas. We have <br></br>
-        software solutions</p>
-          <p className={styles.passionate}>Passionate</p>
-        </div>
-          <div className={styles.lightimage}>
-            <img src={lightImg} alt="LightBulb" />
-
-        </div>
-            
-      </div>
-        <div className={styles.conatiner2}>
-          <div className={styles.phoneimage }>
-            <img src="/src/assets/Frame 201.png" alt="PhoneImage" className={styles.image1} />
-
-          </div>
-            <div className={styles.text2}>
-              <h1 className={styles.title}>Solutions</h1>
-                <p className={styles.paragraph}>
-                We aim to provide each individual service to a whole host of industries.
-                <br></br>Our expertise allow us to fully customise each
-                <br></br> product to work for every individual business model - providing a strong foundation with which any business can begin
-                <br></br> to maximise on their potential.</p>
-              <button className={styles.experties}>Experties</button>
-        </div>
-        </div>
+     
       {/* Ozias Framework (The End) */}
     
       {/* <div className={styles.conatiner}>
@@ -132,7 +98,43 @@ export default function About_Us() {
       </div>
 
       {/* Slide 3 */}
-      <div className={`${styles.aboutDiv} ${styles.div3}`}></div>
+      <div className={`${styles.aboutDiv} ${styles.div3}`}>
+         <div className={styles.container1}>
+        <div className={styles.text}>
+        <h5>Team</h5>
+        <p className={styles.teamText}>
+        Active Media consists of a team of<br></br>
+        people that are passionate about what<br></br>   
+        they do and strive to ensure that only only the<br></br>
+        very best service is offered to our <br></br>
+        clients. We stand by our company ethics<br></br>
+        -ensuring honesty, authenticity and <br></br>
+        excellence. You have ideas. We have <br></br>
+        software solutions</p>
+          <p className={styles.passionate}>Passionate</p>
+        </div>
+          <div className={styles.lightimage}>
+            <img src={lightImg} alt="LightBulb" />
+
+        </div>
+            
+      </div>
+        <div className={styles.container1}>
+          <div className={styles.phoneImage}>
+            <img src="/src/assets/Frame 201.png" alt="PhoneImage" className={styles.image1} />
+
+          </div>
+            <div className={styles.text2}>
+              <h1 className={styles.title}>Solutions</h1>
+                <p className={styles.paragraph}>
+                We aim to provide each individual service to a whole host of industries.
+                <br></br>Our expertise allow us to fully customise each
+                <br></br> product to work for every individual business model - providing a strong foundation with which any business can begin
+                <br></br> to maximise on their potential.</p>
+              <button className={styles.experties}>Experties</button>
+        </div>
+        </div>
+      </div>
 
       {/* Slide 4 */}
       <div className={`${styles.aboutDiv} ${styles.div4}`}></div>
