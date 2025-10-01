f3503e13f1d54afa4346d2ca94a3b74e9dbddc37
Divine Ntambwe <divinentambwe28@gmaail.com>
2025-10-01 13:17:10 +0200

    fixed spacing

--- src/pages/About_Us.jsx
+++ src/pages/About_Us.jsx
@@ -191,7 +191,7 @@ export default function About_Us() {
                 <br></br>Our expertise allow us to fully customise each
                 <br></br> product to work for every individual business model - providing a strong foundation with which any business can begin
                 <br></br> to maximise on their potential.</p>
-              <button className={styles.passionate}>Experties</button>
+              <p className={styles.experties}>Experties</p>
         </div>
         </div>
       </div>
