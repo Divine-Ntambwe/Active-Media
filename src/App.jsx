633cb258a817b8d8f3919688c10096fe334f9585
samuelmbaya <plaatjiessamuel98@gmail.com>
2025-10-01 13:36:33 +0200

    finished rhaddock page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import About_Us from "./pages/About_Us";
 import Contact_Us from "./pages/Contact_Us";
+import Recent_Work from "./pages/Recent_Work";
 import Cloud_Based_Solutions from "./pages/Cloud_Based_Solutions";
 import Navbar from "./component/Navbar";
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<About_Us />} />
           <Route path="/contact-us" element={<Contact_Us />} />
+          <Route path="/recent-work" element={<Recent_Work />} />
         </Routes>
     </Router>
     </>
